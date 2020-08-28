9f2cbc09b9aa9bc3da1d1dc9e90d092f3fc7cfc1
bugsounet <bugsounet@bugsnana.fr>
2020-08-28 04:11:17 +0200

    no prompt code

--- installer/update.sh
+++ installer/update.sh
@@ -34,20 +34,34 @@ Installer_dir="$(Installer_get_current_dir)"
 # move to installler directory
 cd "$Installer_dir"
 source utils.sh
+if $prompt; then
 Installer_info "Welcome to GA updater !"
 echo
+fi
 
 cd ~/MagicMirror/modules/MMM-GoogleAssistant
 # deleting package.json because npm install add/update package
 rm -f package.json package-lock.json
+
+if $prompt; then
 Installer_info "Updating..."
+fi
+
 git pull
 #fresh package.json
 git checkout package.json
 cd ~/MagicMirror/modules/MMM-GoogleAssistant/node_modules
+
+if $prompt; then
 Installer_info "Deleting ALL @bugsounet libraries..."
+fi
+
 rm -rf @bugsounet
 cd ~/MagicMirror/modules/MMM-GoogleAssistant
+
+if $prompt; then
 Installer_info "Ready for Installing..."
+fi
+
 # launch installer
 npm install
