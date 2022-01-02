39c5a09affdc025962480bbbd045bd06fa18a9e8
bugsounet <github@bugsounet.fr>
2022-01-02 12:05:00 +0100

    review Installer for MM v2.18.x

--- installer/postinstall.sh
+++ installer/postinstall.sh
@@ -139,6 +139,11 @@ node minify.js
 Installer_success "Done"
 echo
 
+Installer_info "MagicMirror Rebuild..."
+cd ..
+./node_modules/.bin/MagicMirror-rebuild
+echo
+
 # the end...
 Installer_warning "Support is now moved in a dedicated Server: http://forum.bugsounet.fr"
 Installer_warning "@bugsounet"
--- installer/preinstall.sh
+++ installer/preinstall.sh
@@ -40,5 +40,56 @@ if [ "$EUID" -eq 0 ]; then
   exit 1
 fi
 
+Installer_chk "$(pwd)/../" "MMM-GoogleAssistant"
+Installer_chk "$(pwd)/../../../" "MagicMirror"
+cd ..
 echo
-Installer_info "Installing all npm libraries..."
+
+# Check platform compatibility
+Installer_info "Checking OS..."
+Installer_checkOS
+if  [ "$platform" == "osx" ]; then
+  Installer_error "OS Detected: $OSTYPE ($os_name $os_version $arch)"
+  Installer_error "Automatic installation is not included"
+  echo
+  exit 255
+else
+  if  [ "$os_name" == "raspbian" ] && [ "$os_version" -lt 10 ]; then
+    Installer_error "OS Detected: $OSTYPE ($os_name $os_version $arch)"
+    Installer_error "Unfortunately, this module is not compatible with your OS"
+    Installer_error "Try to update your OS to the lasted version of raspbian"
+    echo
+    exit 255
+  else
+    Installer_success "OS Detected: $OSTYPE ($os_name $os_version $arch)"
+  fi
+fi
+
+echo
+# check dependencies
+dependencies=(wget unclutter build-essential vlc libmagic-dev libatlas-base-dev cec-utils libudev-dev)
+Installer_info "Checking all dependencies..."
+Installer_check_dependencies
+Installer_success "All Dependencies needed are installed !"
+
+echo
+# apply @sdetweil fix
+Installer_info "Installing @sdetweil sandbox fix..."
+bash -c "$(curl -sL https://raw.githubusercontent.com/sdetweil/MagicMirror_scripts/master/fixsandbox)"
+
+echo
+# switch branch
+Installer_info "Installing Sources..."
+if is_pifour; then
+  Installer_info "Raspberry Pi4 Detected."
+else
+  Installer_error "[WARN] You don't use a Raspberry Pi4, switch to light sources..."
+  git checkout -f light 2>/dev/null || Installer_error "Installing Error !"
+  git pull 2>/dev/null
+  Installer_error "~~~~~~"
+  Installer_error "~~~ Retry again npm install for continue installing ~~~"
+  Installer_error "~~~~~~"
+  exit 255
+fi
+
+echo
