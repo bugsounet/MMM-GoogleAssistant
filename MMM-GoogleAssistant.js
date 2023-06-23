66d283b49dee85e6cc7228c657c150da09cf2fdf
bugsounet <github@bugsounet.fr>
2023-06-23 14:58:11 +0200

    (prepare) new requirement

--- MMM-GoogleAssistant.js
+++ MMM-GoogleAssistant.js
@@ -8,7 +8,7 @@
 logGA = (...args) => { /* do nothing */ }
 
 Module.register("MMM-GoogleAssistant", {
-  requiresVersion: "2.22.0",
+  requiresVersion: "2.23.0",
   defaults: {
     debug:false,
     stopCommand: "stop",
