eb8e8ff01d807cb60203d3aa828d5cb138e2ed90
bugsounet <github@bugsounet.fr>
2023-06-30 12:31:48 +0200

    add zh-cn translation

--- MMM-GoogleAssistant.js
+++ MMM-GoogleAssistant.js
@@ -1,14 +1,14 @@
 /**
- ** Module : MMM-GoogleAssistant v5
+ ** Module : MMM-GoogleAssistant
  ** @bugsounet
- ** ©03-2023
+ ** ©06-2023
  ** support: https://forum.bugsounet.fr
  **/
 
 logGA = (...args) => { /* do nothing */ }
 
 Module.register("MMM-GoogleAssistant", {
-  requiresVersion: "2.22.0",
+  requiresVersion: "2.23.0",
   defaults: {
     debug:false,
     stopCommand: "stop",
@@ -84,7 +84,8 @@ Module.register("MMM-GoogleAssistant", {
       nl: "translations/nl.json",
       pt: "translations/pt.json",
       ko: "translations/ko.json",
-      el: "translations/el.json"
+      el: "translations/el.json",
+      "zh-cn": "translations/zh-cn.json"
     }
   },
 
